2af88223a562101bd4062059ba712d6b764cabce
sabialhyv <sabialhyv@gmail.com>
2024-10-28 15:48:00 +0400

    modify

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import logo from '../assets/images/logo.png'
 function Navbar() {
     return (
         <nav>
+            <div className="nav-logo">
+                <img src={logo} alt="" />
+            </div>
+            
             <div className="navbar">
                 <Link to='/'>Home</Link>
                 <Link to='/about'>About</Link>
