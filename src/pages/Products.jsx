2c52205af8e30e1228eeaf36128cd7ec2fc0a4f2
sabialhyv <sabialhyv@gmail.com>
2024-10-29 16:48:55 +0400

    modify

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 function Products() {
   return (
-    <div>Products</div>
+    <div className="products">
+      <h1>Our Products</h1>
+      <div className="container">
+        <div className="product">
+          <img src="https://example.com/product1.jpg" alt="" />
+          <h2>Product 1</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div className="product">
+          <img src="https://example.com/product2.jpg" alt="" />
+          <h2>Product 2</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div className="product">
+          <img src="https://example.com/product3.jpg" alt="" />
+          <h2>Product 3</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
