9b04dfbb8a009a3628a8c2eb65b962072a8627ac
sabialhyv <sabialhyv@gmail.com>
2024-10-31 16:37:38 +0400

    modify

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,37 @@
 import logo from '../assets/images/logo.png';
 import { FaFacebook, FaYoutube, FaPinterest } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
+import video from '../assets/images/tk.gif'
 
 function Footer() {
     return (
-        <footer className='py-14 px-20  bg-yellow-300 w-[85%] mx-[15%] rounded-tl-[85px] flex flex-col items-center' >
-            <div className="footer-text flex w-full justify-between ">
-                <div className="footer-logo">
-                    <img src={logo} className='w-[240px]' />
+        <footer className='py-14 px-20  bg-red-200 w-[85%] mx-[15%] rounded-tl-[85px]' >
+            <div className="flex w-full justify-between items-center">
+                <div className='flex flex-col'>
+                    <p className='max-w-[700px] text-5xl tracking-[10px]'>we are here <span className='text-yellow-400'>to bake</span> happy you!</p>
+                    <div className='flex justify-between mt-10'>
+                        <p>Follow us</p>
+                        <img src={video}/>
+                        <div className="social-media flex justify-between items-center gap-3">
+                            <a className='bg-red-300 rounded-full p-3' href="#"><FaFacebook className='text-2xl text-white' /></a>
+                            <a className='bg-red-300 rounded-full p-3' href="#"><FaPinterest className='text-2xl text-white' /></a>
+                            <a className='bg-red-300 rounded-full p-3' href="#"><AiFillInstagram className='text-2xl text-white' /></a>
+                            <a className='bg-red-300 rounded-full p-3' href="#"><FaYoutube className='text-2xl text-white' /></a>
+                        </div>
+                        <div className="flex flex-col gap-3 tracking-widest items-center gap-3">
+                            <p className='font-bold'>Location</p>
+                            <p>123 Main St, Anytown, USA</p>
+                            <p className='font-bold'>Contact Phone</p>
+                            <p>852 452 74 78</p>
+                        </div>
+                    </div>
                 </div>
-                <div className="social-media flex justify-between items-center gap-3">
-                    <a href="#"><FaFacebook className='text-3xl text-red-300' /></a>
-                    <a href="#"><FaPinterest className='text-3xl text-red-300' /></a>
-                    <a href="#"><AiFillInstagram className='text-3xl text-red-300' /></a>
-                    <a href="#"><FaYoutube className='text-3xl text-red-300' /></a>
-                </div>
-                <div className="footer-location">
-                    <span>Location:</span>
-                    <p>123 Main St, Anytown, USA</p>
-                </div>
+                <img src={logo} className='w-[300px] h-[260px]' />
             </div>
+
+
+
+
             <p className='text-center mt-4 text-white text-lg'> 2024 My Website. All rights reserved.</p>
         </footer>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
     <div className='home w-full tracking-widest p-20 relative'>
       <h1 className='text-red-300 text-7xl my-3'>Maison Fraise</h1>
       <h4 className='text-5xl text-yellow-500'>Welcome to our sweet pastry shop!</h4>
-      <p className="slogan absolute top-[60%] left-[50%] max-w-[300px] text-4xl tracking-[10px]">
+      <p className="absolute top-[60%] left-[50%] max-w-[300px] text-4xl tracking-[10px]">
         we are here to bake happy you!
       </p>
     </div>
