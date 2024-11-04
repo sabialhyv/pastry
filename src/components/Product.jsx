9e74abed0b83c228163d2acd41fe67c7a58fa602
sabialhyv <sabialhyv@gmail.com>
2024-11-04 11:00:21 +0400

    modify

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Product({ name, url, description }) {
+function Product({ cake }) {
     return (
         <div className="w-1/3 m-5 p-5 bg-[#f6f4f4] rounded-md shadow">
-            <img src={url}  alt={name} className='rounded-md h-60 object-cover w-full'  />
-            <h2 className='text-yellow-500 text-2xl font-bold my-2'>{name}</h2>
-            <p className='text-lg'>{description}</p>
+            <img src={`../src/assets/images/${cake.url}`}  alt={cake.name} className='rounded-md h-60 object-cover w-full'  />
+            <h2 className='text-yellow-500 text-2xl font-bold my-2'>{cake.name}</h2>
+            <p className='text-lg'>{cake.description}</p>
         </div>
     )
 }
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
-import Product from '../components/Product'
-import choco from '../assets/images/chococake.webp'
-import vanilla from '../assets/images/vanillacake.jpg'
-import velvet from '../assets/images/redcake.jpg'
-
+import React, { useEffect, useState } from 'react';
+import Product from '../components/Product';
 
 function Products() {
- 
+  const [cakes, setCakes] = useState([]);
+
+  useEffect(() => {
+    fetch('data.json')
+      .then(response => response.json())
+      .then(data => {
+        setCakes(data);
+      })
+      .catch(error => console.error('Error fetching JSON:', error));
+  }, []);
   return (
     <>
-      <h1 className='text-center font-bold text-6xl text-red-300 my-2'>Our Products</h1>
+      <h1 className="text-center font-bold text-6xl text-red-300 my-2">Our Products</h1>
       <div className="flex">
-        {products && products.map(product => (
-          <Product key={product.id} {...product} />
-        ))}
+        {
+          cakes.map(cake => (
+            <Product key={cake.id} cake={cake} />
+          ))
+        }
       </div>
     </>
-  )
+  );
 }
 
-export default Products
+export default Products;
