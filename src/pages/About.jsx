bddde80016ecae1b1c8630e03b4f857931a76662
sabialhyv <sabialhyv@gmail.com>
2024-11-05 16:14:51 +0400

    modify

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,8 @@
-import React from 'react'
-
+import img1 from '../assets/images/'
 function About() {
   return (
-    <div className="about">
-      <h1>About Us</h1>
-      <div className="about-info">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non quam lectus, auctor vitae dui non, gravida sagittis ipsum. Nulla facilisi. Sed vel enim ut dui pharetra posuere vel vitae arcu. Sed consectetur, quam vel facilisis semper, velit nulla rhoncus neque, sed consectetur neque velit at velit. Nulla facilisi. Sed vel enim ut dui pharetra posuere vel vitae arcu. Sed consectetur, quam vel facilisis semper, velit nulla rhoncus neque, sed consectetur neque velit at velit.</p>
-      </div>
-      <div className="about-image">
-        <img src="" alt="" />
-      </div>
+    <div className="about bg-">
+
     </div>
   )
 }
