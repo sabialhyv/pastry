328d72b0a7ae1d3dbea6ca455693c9b1d1e7faf7
sabialhyv <sabialhyv@gmail.com>
2024-11-05 17:39:59 +0400

    modify

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,6 +5,11 @@ import img2 from '../assets/images/2.jpeg'
 import img3 from '../assets/images/3.jpeg'
 import img4 from '../assets/images/4.jpeg'
 import img6 from '../assets/images/6.jpeg'
+import img7 from '../assets/images/7.webp'
+import img8 from '../assets/images/8.webp'
+import img9 from '../assets/images/9.webp'
+
+
 
 
 function About() {
