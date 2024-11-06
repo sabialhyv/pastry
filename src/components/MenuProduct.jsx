ec5b4f27a448e5ea1db9fa3a479570af96d327d6
sabialhyv <sabialhyv@gmail.com>
2024-11-06 15:42:07 +0400

    modify

--- src/components/MenuProduct.jsx
+++ src/components/MenuProduct.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import 
 
 function MenuProduct() {
   return (
     <div>
-        
+      <div>
+        <img src="" alt="" />
+        <p></p>
+      </div>
+      <div>
+        <img src="" alt="" />
+        <p></p>
+      </div>
+      <div>
+        <img src="" alt="" />
+        <p></p>
+      </div>
+      <div>
+        <img src="" alt="" />
+        <p></p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default MenuProduct
+export default MenuProduct;
