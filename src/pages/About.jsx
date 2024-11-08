9fc5b3a5b32b1d50942a5a768f057bc033ce5c3c
sabialhyv <sabialhyv@gmail.com>
2024-11-08 12:15:46 +0400

    modify

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 import shop from "../assets/images/shop.png";
+import { motion } from "framer-motion";
+
 import img1 from "../assets/images/1.jpeg";
 import img2 from "../assets/images/2.jpeg";
 import img3 from "../assets/images/3.jpeg";
@@ -16,19 +18,49 @@ function About() {
     <div className="about-section p-20 min-h-96 w-[70%] m-auto">
       <div className="text-center">
         <h1 className="text-9xl mb-4">Maison Fraise</h1>
-        <p className="menu-text text-start italic text-5xl ml-14">pâtisserie</p>
+        <motion.p
+          className="menu-text text-start italic text-5xl ml-14"
+          initial={{ x: -200 }}
+          animate={{ x: 0 }}
+          transition={{
+            duration: 2,
+            loop: Infinity,
+          }}
+        >
+          pâtisserie
+        </motion.p>
       </div>
       <div className=" menu-text flex items-end gap-20 justify-between">
         <div className="flex flex-col gap-10">
-          <p className="uppercase text-lg">
+          <motion.p
+            className="uppercase text-lg"
+            initial={{ x: -200 }}
+            animate={{ x: 0 }}
+            transition={{
+              duration: 2,
+              loop: Infinity,
+            }}
+          >
             WELCOME TO OUR PASTRY SHOP Lorem, ipsum dolor sit amet consectetur
             adipisicing elit. Error vitae eligendi enim dolorum, a, nihil,
             ducimus modi aperiam autem accusantium rem sit maiores libero
             doloremque qui illo!
-          </p>
-          <Link  className=" text-lg underline italic" to="/menu">See the menu</Link>
+          </motion.p>
+          <Link className=" text-lg underline italic" to="/menu">
+            See the menu
+          </Link>
         </div>
-        <img className="w-[40%] h-[450px] -mt-[49px]" src={shop} alt="" />
+        <motion.img
+          className="w-[40%] h-[450px] -mt-[49px]"
+          initial={{ x: 200 }}
+          animate={{ x: 0 }}
+          transition={{
+            duration: 2,
+            loop: Infinity,
+          }}
+          src={shop}
+          alt=""
+        />
       </div>
     </div>
   );
