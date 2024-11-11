c0a78a3c709448e9c586063769e24ab4ae983c40
sabialhyv <sabialhyv@gmail.com>
2024-11-11 14:51:30 +0400

    modify

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,15 +3,6 @@ import { useRef } from "react"
 import { Link } from "react-router-dom"
 import shop from "../assets/images/shop.png"
 
-import img1 from "../assets/images/1.jpeg"
-import img2 from "../assets/images/2.jpeg"
-import img3 from "../assets/images/3.jpeg"
-import img4 from "../assets/images/4.jpeg"
-import img6 from "../assets/images/6.jpeg"
-import img7 from "../assets/images/7.webp"
-import img8 from "../assets/images/8.webp"
-import img9 from "../assets/images/9.webp"
-
 function About() {
   const ref = useRef(null)
   const isInView = useInView(ref, { once: false })
