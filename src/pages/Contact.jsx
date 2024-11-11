1a5211ac6e507beb93757e4142820c877ae39c10
sabialhyv <sabialhyv@gmail.com>
2024-11-11 15:24:17 +0400

    modify

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,10 +2,23 @@ import React from 'react'
 
 function Contact() {
   return (
-    <section className="contact-section p-20">
-      <h2 className="text-4xl mb-4">Contact Us</h2>
-      <p>We would love to hear from you!</p>
-    </section>
+    <div className='m-auto w-[50%] mb-24' >
+    <div className="contact-section p-20 text-center">
+      <h1 className="text-7xl mb-4">Contact Us</h1>
+      <p className='menu-text italic  text-2xl '>We would love to hear from you!</p>
+    </div>
+    <form action="" className='flex flex-col m-auto gap-3'>
+      <label htmlFor="" className='text-2xl menu-text '>Name</label>
+      <input type="text" className='h-8 bg-transparent outline-none border-b-2 mt-2 border-yellow-500' />
+      <label htmlFor="" className='text-2xl menu-text '>Surname</label>
+      <input type="text" className='h-8 bg-transparent outline-none border-b-2 mt-2 border-yellow-500'/>
+      <label htmlFor="" className='text-2xl menu-text '>Email</label>
+      <input type="email" className='h-8 bg-transparent outline-none border-b-2 mt-2 border-yellow-500' />
+      <label htmlFor="" className='text-2xl menu-text '>Phone Number</label>
+      <input type="text" className='h-8 bg-transparent outline-none  border-b-2 mt-2 border-yellow-500' />
+    </form>
+    
+    </div>
   )
 }
 
